6e3d1af7dd7cccde494738be0ce26d38e4426429
Jude <jude@platform45.com>
2015-10-23 16:06:30 +0200

    feat: menu design

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'homepage/index'
+  get 'homepage/show'
+
+  root to: redirect("homepage/show")
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
